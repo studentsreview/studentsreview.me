d2541cbcbd28b55587fea8a16a783834a538d068
Kai Chang <kaijchang@gmail.com>
2019-05-27 08:21:05 -0700

    feat: add analytics and 404 page

--- app/gatsby-config.js
+++ app/gatsby-config.js
@@ -4,7 +4,18 @@ module.exports = {
             resolve: `gatsby-source-mongodb`,
             options: { dbName: `StudentsReview`, collection: [`classes`, `reviews`] },
         },
+        {
+            resolve: `gatsby-plugin-google-analytics`,
+            options: {
+                trackingId: `UA-140917974-1`,
+            }
+        },
         `gatsby-plugin-jss`,
-        `gatsby-plugin-material-ui`
-    ]
+        `gatsby-plugin-material-ui`,
+        `gatsby-plugin-remove-trailing-slashes`
+    ],
+    proxy: {
+        prefix: `/api`,
+        url: `http://localhost:8080`
+    }
 }
---
+++ app/src/pages/404.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Layout from '../components/layout';
+
+const NotFoundPage = () => <Layout gridStyle={ {
+    minHeight: '70%'
+} }>
+    <h3>Are You Lost?</h3>
+</Layout>;
+
+export default NotFoundPage;
