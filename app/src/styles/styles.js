e82f2a5dada54d02f393d2c9de683fc2156eb375
Kai Chang <kaijchang@gmail.com>
2019-05-26 18:17:54 -0700

    fix: actually card size

--- app/src/styles/styles.js
+++ app/src/styles/styles.js
@@ -5,9 +5,9 @@ export default theme => createStyles({
         padding: 15,
         margin: 20,
         width: 600,
-        maxWidth: 'calc(100vw - 70px)',
+        maxWidth: 'calc(100vw - 40px)',
         [theme.breakpoints.up('md')]: {
-            maxWidth: 'calc(50vw - 70px)'
+            maxWidth: 'calc(50vw - 40px)'
         }
     }
 });
