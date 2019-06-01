39d928b0090e82f81839f37424f908997f914119
Kai Chang <kaijchang@gmail.com>
2019-06-01 14:03:55 -0700

    fix: submit review url

--- app/src/components/ReviewForm.js
+++ app/src/components/ReviewForm.js
@@ -92,7 +92,7 @@ const ReviewForm = ({ classes, teacher, onClose, theme }) => {
                 />
                 <br/>
                 <Button disabled={ reviewText.length < minCharacters && starRating > 0 } onClick={ () => {
-                    axios.post('api.studentsreview.me/submitreview', {
+                    axios.post('https://api.studentsreview.me/submitreview', {
                         teacher: teacher,
                         text: reviewText,
                         rating: starRating
