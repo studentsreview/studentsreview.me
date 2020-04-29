2e514f1f45ec7a70660fd052c04b9c3726bb5c3d
Kai Chang <kaijchang@gmail.com>
2020-04-28 21:35:49 -0700

    .

--- arena/visualize.py
+++ arena/visualize.py
@@ -7,7 +7,7 @@ with open('liveseats.json') as live_seats_file:
     live_seats_data = json.load(live_seats_file)
 
 for course in live_seats_data:
-    if course['teacher'] != 'Fortin':
+    if int(course['timeSeries'][0]['seats']) > 50:
         continue
     plt.plot(
         [dateutil.parser.parse(series['time']) for series in course['timeSeries']],
