6b032fc90009659502ef34781ca3edb095072f58
Kai Chang <kaijchang@gmail.com>
2020-05-04 13:14:11 -0700

    live seats

--- arena/liveseats.js
+++ arena/liveseats.js
@@ -1,54 +1,38 @@
 const axios = require('axios');
+const moment = require('moment');
 
 const fs = require('fs');
+const path = require('path');
 
-const data = [];
+const timetable = {
+    1: moment('May 4th 2020, 2:30 pm', 'MMMM Do YYYY, h:mm a'),
+    2: moment('May 4th 2020, 3:30 pm', 'MMMM Do YYYY, h:mm a'),
+    3: moment('May 4th 2020, 4:30 pm', 'MMMM Do YYYY, h:mm a'),
+    4: moment('May 4th 2020, 5:30 pm', 'MMMM Do YYYY, h:mm a'),
+    5: moment('May 4th 2020, 6:30 pm', 'MMMM Do YYYY, h:mm a'),
+    6: moment('May 6th 2020, 2:30 pm', 'MMMM Do YYYY, h:mm a'),
+    7: moment('May 6th 2020, 3:30 pm', 'MMMM Do YYYY, h:mm a'),
+    8: moment('May 6th 2020, 4:30 pm', 'MMMM Do YYYY, h:mm a'),
+    9: moment('May 6th 2020, 5:30 pm', 'MMMM Do YYYY, h:mm a'),
+    10: moment('May 6th 2020, 6:30 pm', 'MMMM Do YYYY, h:mm a'),
+    11: moment('May 8th 2020, 3:00 pm', 'MMMM Do YYYY, h:mm a'),
+    12: moment('May 8th 2020, 4:00 pm', 'MMMM Do YYYY, h:mm a')
+};
 
-function writeToFile() {
-    fs.writeFileSync('liveseats.json', JSON.stringify(data));
+let next_rotation = 1;
+
+const checkForRotation = () => {
+    if (Math.abs(moment().diff(timetable[next_rotation])) < 1000) {
+        axios.get('https://arenarolodexscraper.herokuapp.com/')
+            .then(res => {
+                fs.writeFileSync(path.join(__dirname, 'dumps', next_rotation + '.json'), JSON.stringify(res.data));
+                next_rotation++;
+                setTimeout(checkForRotation, 100);
+            });
+    } else {
+        setTimeout(checkForRotation, 100);
+    }
 }
 
-axios.get('https://us-central1-lowellarenarolodex.cloudfunctions.net/liveseats')
-    .then(res => {
-        console.log('Fetching ' + (new Date()).toISOString());
-        for (let [department, classes] of Object.entries(res.data)) {
-            for (let [className, teachers] of Object.entries(classes)) {
-                for (let [teacher, blocks] of Object.entries(teachers)) {
-                    for (let block of blocks) {
-                        data.push({
-                            department,
-                            className,
-                            teacher,
-                            block: block[0],
-                            timeSeries: [{ time: (new Date()).toISOString(), seats: block[2] }]
-                        });
-                    }
-                }
-            }
-        }
-        writeToFile();
-        setInterval(() => {
-            console.log('Fetching ' + (new Date()).toISOString());
-            axios.get('https://us-central1-lowellarenarolodex.cloudfunctions.net/liveseats')
-                .then(res => {
-                    for (let [department, classes] of Object.entries(res.data)) {
-                        for (let [className, teachers] of Object.entries(classes)) {
-                            for (let [teacher, blocks] of Object.entries(teachers)) {
-                                for (let block of blocks) {
-                                    const match = data.find(datum => Object.entries({
-                                        department,
-                                        className,
-                                        teacher,
-                                        block: block[0]
-                                    }).every(entry => datum[entry[0]] === entry[1]));
-                                    if (match.timeSeries[match.timeSeries.length - 1].seats !== block[2]) {
-                                        match.timeSeries.push({ time: (new Date()).toISOString(), seats: block[2] });
-                                    }
-                                }
-                            }
-                        }
-                    }
-                    writeToFile();
-                });
-        }, 1000);
-    });
+checkForRotation();
+
