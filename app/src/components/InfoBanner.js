bd4f0d8ca78916d050deb0682f9db90b171d67d6
Kai Chang <kaijchang@gmail.com>
2020-06-23 15:40:35 -0700

    add announcement

--- app/src/components/InfoBanner.js
+++ app/src/components/InfoBanner.js
@@ -6,10 +6,16 @@ import { OutboundLink } from 'gatsby-plugin-google-analytics';
 import { useTheme } from '@material-ui/styles';
 
 const InfoBanner = () => {
-    // const theme = useTheme();
+    const theme = useTheme();
 
     return (
-        null
+        <Paper style={ { margin: theme.spacing(2), padding: theme.spacing(2), background: theme.palette.secondary.main } }>
+            <OutboundLink href='https://docs.google.com/document/d/15zwMQ6gAGuwqTceJjWFbxyB_GKfX4n-YRpWtaXBgcVE/edit' target='_blank' rel='noopener noreferrer'>
+                <Typography variant='body1' align='center'>
+                    Read about SA/SH Accusations Against Lowell Teachers
+                </Typography>
+            </OutboundLink>
+        </Paper>
     );
 }
 
