748e9b00f6b26dfc4ca2366616da1d86d1d45d88
Kai Chang <kaijchang@gmail.com>
2020-09-03 21:37:58 -0700

    remove banner

--- app/src/components/InfoBanner.js
+++ app/src/components/InfoBanner.js
@@ -9,13 +9,7 @@ const InfoBanner = () => {
     const theme = useTheme();
 
     return (
-        <Paper style={ { margin: theme.spacing(2), padding: theme.spacing(2), background: theme.palette.secondary.main } }>
-            <OutboundLink href='https://docs.google.com/document/d/15zwMQ6gAGuwqTceJjWFbxyB_GKfX4n-YRpWtaXBgcVE/edit' target='_blank' rel='noopener noreferrer'>
-                <Typography variant='body1' align='center'>
-                    Read about SA/SH Accusations Against Lowell Teachers
-                </Typography>
-            </OutboundLink>
-        </Paper>
+        null
     );
 }
 
